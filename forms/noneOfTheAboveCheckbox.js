9938a9bcb881b8d04f31e7960e23dc53a526ac0b
fauverism <robertfauver@gmail.com>
2014-12-17 16:59:47 -0500

    adding logic for an instance of more than one checkbox group appearing on the page

--- forms/noneOfTheAboveCheckbox.js
+++ forms/noneOfTheAboveCheckbox.js
@@ -15,6 +15,24 @@ if($scope.answertype==='CHECKBOX') {
   },100);
 }
 
+//For multiple checkboxes appearing on the same page
+if($scope.answerType==='CHECKBOX') {
+  $timeout(function() {
+    $('.none-of-the-above').change(function() {
+      var x = $(this);
+      var isChecked = x.is(':checked');
+      if (x.hasClass('none-of-the-above') && isChecked) {
+        x.parents('ul.m_choices li').siblings().find('input').attr('checked', false);
+      } else if ( (x.hasClass('normal')) && !isChecked ) {
+        x.parents('ul li').siblings().find('input.none-of-the-above').attr('checked', false);
+      }
+    });
+    $('.normal').change(function() {
+      $(this).parents('ul li').siblings().find('input.none-of-the-above').attr('checked', false);
+    });
+  },100);
+}
+
 /*
 This code is used in order to have a 'None of the Above' that deselects all checkboxes in the checkbox group. This also removes the 'None of the Above' :checked if another checkbox is checked while 'None of the Above' is checked.
 */
